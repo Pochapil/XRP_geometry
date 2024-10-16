deaadf3ff73fc26560867bfc00cb3e725309973d
Pochapil <bagrat37@mail.ru>
2024-10-16 14:18:09 +0300

    some indexing

--- tests_with_old/test_cos_arr.py
+++ tests_with_old/test_cos_arr.py
@@ -1,12 +1,12 @@
 '''normals'''
 # array_normal = np.zeros((phi_arr.shape[0], theta_arr.shape[0]), dtype=object)
 # count = 1
-# for i in range(phi_arr.shape[0]):
-#     for j in range(theta_arr.shape[0]):
-#         # array_normal.append(coefficient * matrix.newE_n(phi_range[i], theta_range[j]))
-#         # array_normal[i, j] = 1 * matrix.newE_r(phi_arr[i], theta_arr[j])
-#         array_normal[i, j] = 1 * matrix.newE_n(phi_arr[i], theta_arr[j])
-#         print(array_normal[i, j] - z[i, j, :])
+# for phi_index in range(phi_arr.shape[0]):
+#     for theta_index in range(theta_arr.shape[0]):
+#         # array_normal.append(coefficient * matrix.newE_n(phi_range[phi_index], theta_range[theta_index]))
+#         # array_normal[phi_index, theta_index] = 1 * matrix.newE_r(phi_arr[phi_index], theta_arr[theta_index])
+#         array_normal[phi_index, theta_index] = 1 * matrix.newE_n(phi_arr[phi_index], theta_arr[theta_index])
+#         print(array_normal[phi_index, theta_index] - z[phi_index, theta_index, :])
 #         print(count)
 #         count += 1
 # # print(array_normal)
@@ -15,21 +15,21 @@
 
 '''cos'''
 # cos_psi_range = np.empty([config.N_phi_accretion, config.N_theta_accretion])
-# for i in range(config.N_phi_accretion):
-#     for j in range(config.N_theta_accretion):
+# for phi_index in range(config.N_phi_accretion):
+#     for theta_index in range(config.N_theta_accretion):
 #         # умножать на N_theta
-#         # cos_psi_range[i, j] = np.dot(e_obs_mu, self.array_normal[i * config.N_theta_accretion + j])
-#         cos_psi_range[i, j] = np.dot(obs_matrix[6], surf.array_normal[i][j])
+#         # cos_psi_range[phi_index, theta_index] = np.dot(e_obs_mu, self.array_normal[phi_index * config.N_theta_accretion + theta_index])
+#         cos_psi_range[phi_index, theta_index] = np.dot(obs_matrix[6], surf.array_normal[phi_index][theta_index])
 #
 # print(rotation_obs_matrix - cos_psi_range)
 
 '''in cycle! all empty'''
-# for k in range(config.N_phase):
+# for phase_index in range(config.N_phase):
 #     cos_psi_range = np.empty([config.N_phi_accretion, config.N_theta_accretion])
-#     for i in range(config.N_phi_accretion):
-#         for j in range(config.N_theta_accretion):
+#     for phi_index in range(config.N_phi_accretion):
+#         for theta_index in range(config.N_theta_accretion):
 #             # умножать на N_theta
-#             # cos_psi_range[i, j] = np.dot(e_obs_mu, self.array_normal[i * config.N_theta_accretion + j])
-#             cos_psi_range[i, j] = np.dot(obs_matrix[k], surf.array_normal[i][j])
+#             # cos_psi_range[phi_index, theta_index] = np.dot(e_obs_mu, self.array_normal[phi_index * config.N_theta_accretion + theta_index])
+#             cos_psi_range[phi_index, theta_index] = np.dot(obs_matrix[phase_index], surf.array_normal[phi_index][theta_index])
 #
-#     print(rotation_obs_matrix[k][(rotation_obs_matrix[k] - cos_psi_range) > 0])
+#     print(rotation_obs_matrix[phase_index][(rotation_obs_matrix[phase_index] - cos_psi_range) > 0])
