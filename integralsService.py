0bbd7bd8c6258920b2220fb6f751e65436440a3f
Pochapil <bagrat37@mail.ru>
2024-10-18 11:54:22 +0300

    поменял порядок функций + добавил scater L_nu

--- integralsService.py
+++ integralsService.py
@@ -17,34 +17,23 @@ def create_ds_for_integral(surface):
     return tilda_s
 
 
+def calculate_total_luminosity(surface, T_eff):
+    # emission power = integral without cos
+    tensor = np.ones((config.N_phi_accretion, config.N_theta_accretion))
+    return calc_L(surface, T_eff, tensor)
+
+    # tilda_s = create_ds_for_integral(surface)
+    # emission_power = (4 * config.sigm_Stf_Bolc * scipy.integrate.simps(
+    #     scipy.integrate.simps(T_eff ** 4 * tensor * tilda_s, surface.theta_range), surface.phi_range))
+    #
+    # return emission_power
+
+
 def calc_L(surface, T_eff, cos_tensor):
     tilda_s = create_ds_for_integral(surface)
     L = np.abs(4 * config.sigm_Stf_Bolc * scipy.integrate.simps(
         scipy.integrate.simps(T_eff ** 4 * cos_tensor * tilda_s, surface.theta_range), surface.phi_range))
     return L
-
-
-def calc_scatter_L(surface, L_x, cos_tensor, tau_scatter_matrix=None):
-    tilda_s = create_ds_for_integral(surface)
-    d = surface.surf_R_e * np.sin(surface.theta_range) ** 2  # distance to area
-    coeff = 1 / (4 * np.pi * d ** 2)
-    if tau_scatter_matrix is not None:
-        coeff = coeff * (1 - np.exp(-np.abs(tau_scatter_matrix)))  # УЧЕСТЬ tau в отражаемой точке!!!!!!!!!
-        # np.abs ??? хотя вроде бы косинус между нормалью и на НЗ не может быть отрицательным
-        # z = (1 - np.exp(-tau_scatter_matrix)) tau = 45-50 ????
-    L = L_x * np.abs(scipy.integrate.simps(scipy.integrate.simps(cos_tensor * tilda_s * coeff, surface.theta_range),
-                                           surface.phi_range))
-    return L
-
-
-def calculate_total_luminosity(surface, T_eff):
-    # emission power
-    tilda_s = create_ds_for_integral(surface)
-    tensor = np.ones((config.N_phi_accretion, config.N_theta_accretion))
-
-    emission_power = (4 * config.sigm_Stf_Bolc * scipy.integrate.simps(
-        scipy.integrate.simps(T_eff ** 4 * tensor * tilda_s, surface.theta_range), surface.phi_range))
-    return emission_power
 
 
 def calc_L_nu(surface, T_eff, cos_tensor):
@@ -63,6 +52,35 @@ def calc_L_nu(surface, T_eff, cos_tensor):
     return L_nu
 
 
+def calc_scatter_L(surface, L_x, cos_tensor, tau_scatter_matrix=None):
+    tilda_s = create_ds_for_integral(surface)
+    d = surface.surf_R_e * np.sin(surface.theta_range) ** 2  # distance to area
+    coeff = 1 / (4 * np.pi * d ** 2)
+    if tau_scatter_matrix is not None:
+        coeff = coeff * (1 - np.exp(-np.abs(tau_scatter_matrix)))  # УЧЕСТЬ tau в отражаемой точке!!!!!!!!!
+        # np.abs ??? хотя вроде бы косинус между нормалью и на НЗ не может быть отрицательным
+        # z = (1 - np.exp(-tau_scatter_matrix)) tau = 45-50 ????
+    L = L_x * np.abs(scipy.integrate.simps(scipy.integrate.simps(cos_tensor * tilda_s * coeff, surface.theta_range),
+                                           surface.phi_range))
+    return L
+
+
+def calc_scatter_L_nu(surface, T_eff, cos_tensor, tau_scatter_matrix=None):
+    tensor = np.ones((config.N_phi_accretion, config.N_theta_accretion))
+    emission_intensity = calc_L_nu(surface, T_eff, tensor)
+
+    tilda_s = create_ds_for_integral(surface)
+    d = surface.surf_R_e * np.sin(surface.theta_range) ** 2  # distance to area
+    coeff = 1 / (4 * np.pi * d ** 2)
+    if tau_scatter_matrix is not None:
+        coeff = coeff * (1 - np.exp(-np.abs(tau_scatter_matrix)))  # УЧЕСТЬ tau в отражаемой точке!!!!!!!!!
+        # np.abs ??? хотя вроде бы косинус между нормалью и на НЗ не может быть отрицательным
+        # z = (1 - np.exp(-tau_scatter_matrix)) tau = 45-50 ????
+    L_nu = emission_intensity[:, np.newaxis] * np.abs(
+        scipy.integrate.simps(scipy.integrate.simps(cos_tensor * tilda_s * coeff, surface.theta_range),
+                              surface.phi_range))
+    return L_nu
+
 # for energy_index in range(config.N_energy - 1):
 #     current_energy_min = config.energy_arr[energy_index]
 #     current_energy_max = config.energy_arr[energy_index + 1]
@@ -75,25 +93,3 @@ def calc_L_nu(surface, T_eff, cos_tensor):
 # calc shadowed_matrix (ns + columns)
 # tau
 # calc tau_matrix
-
-'''
-np.array([np.sin(theta_obs) * np.cos(phi),
-          np.sin(theta_obs) * np.sin(phi),
-          np.cos(theta_obs)]) - типо куда луч стреляем. нужно для нахождения tau1
-'''
-
-
-# def calc_scatter_L():
-#     ...
-
-
-# save
-# calc nu L nu
-
-def calc_scatter_L_nu():
-    ...
-# save
-# calc nu L nu
-
-# tij -> ti -> t
-# scipy.integrate(axis=-1) --- можно интегрировать по тензору.
