ba26141a38ecd8b741b25f3f3fa5369bfa104c22
Pochapil <bagrat37@mail.ru>
2024-10-18 10:56:26 +0300

    if not shadow column then tau + magnet_surface

--- main.py
+++ main.py
@@ -14,6 +14,8 @@ def calc_shadows_and_tau(curr_configuration, surface, obs_matrix, mask_flag=Fals
     # тензор косинусов между нормалью и направлением на наблюдателя размером phase x phi x theta
     # умножаем скалярно phi x theta x 3 на phase x 3 (по последнему индексу) и делаем reshape.
     cos_psi_rotation_matrix = np.einsum('ijl,tl->tij', surface.array_normal, obs_matrix)
+    # print(f'obs_matrix = {np.max(np.linalg.norm(obs_matrix, axis=1))}')
+    # print(f'array_normal_max = {np.max(np.linalg.norm(surface.array_normal, axis=2))}')
     # возможно хранить будем все матрицы.
     # для разных матриц можем посчитать L и посмотреть какой вклад будет.
     tensor_shadows_NS = np.ones_like(cos_psi_rotation_matrix)
@@ -27,7 +29,7 @@ def calc_shadows_and_tau(curr_configuration, surface, obs_matrix, mask_flag=Fals
         mask = np.zeros_like(new_cos_psi_range).astype(bool)
         mask += surface.mask_array
         new_cos_psi_range[mask] = 0
-
+    # print(surface)
     for phase_index in range(config.N_phase):
         for phi_index in range(config.N_phi_accretion):
             for theta_index in range(config.N_theta_accretion):
@@ -43,19 +45,20 @@ def calc_shadows_and_tau(curr_configuration, surface, obs_matrix, mask_flag=Fals
                                                              obs_matrix[phase_index], solutions,
                                                              curr_configuration.top_column.inner_surface,
                                                              curr_configuration.bot_column.inner_surface)
-
-                        tensor_tau[phase_index, phi_index, theta_index] = \
-                            shadows.get_tau_with_dipole(surface, phi_index, theta_index, obs_matrix[phase_index],
-                                                        solutions, surface.surf_R_e, curr_configuration.beta_mu,
-                                                        curr_configuration.M_accretion_rate,
-                                                        curr_configuration.top_column.inner_surface,
-                                                        curr_configuration.bot_column.inner_surface,
-                                                        curr_configuration.a_portion)
+                        if tensor_shadows_columns[phase_index, phi_index, theta_index] > 0:
+                            tensor_tau[phase_index, phi_index, theta_index] = \
+                                shadows.get_tau_with_dipole(surface, phi_index, theta_index, obs_matrix[phase_index],
+                                                            solutions, curr_configuration.top_column.R_e,
+                                                            curr_configuration.beta_mu,
+                                                            curr_configuration.M_accretion_rate,
+                                                            curr_configuration.top_column.inner_surface,
+                                                            curr_configuration.bot_column.inner_surface,
+                                                            curr_configuration.a_portion)
                 else:
                     new_cos_psi_range[phase_index, phi_index, theta_index] = 0
     new_cos_psi_range = new_cos_psi_range * tensor_shadows_NS * tensor_shadows_columns * tensor_tau
-    if tensor_tau[tensor_tau != 1].shape[0] > 0:
-        print(np.max(tensor_tau[tensor_tau != 1]))
+    # if tensor_tau[tensor_tau != 1].shape[0] > 0:
+    #     print(np.max(tensor_tau[tensor_tau != 1]))
     return new_cos_psi_range
 
 
@@ -71,16 +74,14 @@ if __name__ == '__main__':
 
     theta_obs = 60
 
-    cur_path = pathService.PathSaver(mu, theta_obs, beta_mu, mc2, a_portion, phi_0).get_path()
-    print(cur_path)
+    cur_dir_saved = pathService.PathSaver(mu, theta_obs, beta_mu, mc2, a_portion, phi_0)
+    cur_path = cur_dir_saved.get_path()
+    # print(cur_path)
 
     theta_obs_rad = np.deg2rad(theta_obs)
     beta_mu_rad = np.deg2rad(beta_mu)
 
-    x = np.sin(theta_obs_rad) * np.cos(0)
-    y = np.sin(theta_obs_rad) * np.sin(0)
-    z = np.cos(theta_obs_rad)
-    e_obs = np.array([x, y, z])
+    e_obs = matrix.get_cartesian_from_spherical(1, theta_obs_rad, 0)
 
     # obs_matrix = np.zeros((config.N_theta_accretion, config.N_phi_accretion, 3))
     obs_matrix = np.zeros((config.N_phase, 3))
@@ -101,8 +102,8 @@ if __name__ == '__main__':
                       curr_configuration.bot_column.outer_surface, curr_configuration.bot_column.inner_surface]
 
     T_eff = curr_configuration.top_column.T_eff
-    print(integralsService.calculate_total_luminosity(curr_configuration.top_column.inner_surface, T_eff))
-    print(curr_configuration.top_column.L_x)
+    # print(integralsService.calculate_total_luminosity(curr_configuration.top_column.inner_surface, T_eff))
+    # print(curr_configuration.top_column.L_x)
 
     L = np.empty(4, dtype=object)
     L_nu = np.empty(4, dtype=object)
@@ -126,25 +127,25 @@ if __name__ == '__main__':
     L_nu = np.empty(2, dtype=object)
 
     # ------------------------------------------------ L_scatter ----------------------------------------------------
-    for i, surface in enumerate(magnet_line_surfs):
-        new_cos_psi_range = calc_shadows_and_tau(curr_configuration, surface, obs_matrix, True)
+    for i, magnet_surface in enumerate(magnet_line_surfs):
+        new_cos_psi_range = calc_shadows_and_tau(curr_configuration, magnet_surface, obs_matrix, True)
         print(new_cos_psi_range)
 
         # УЧЕСТЬ угол падения от центра в отражаемой точке!!!!!!!!!
-        xyz_magnet_line = matrix.get_xyz_coord(surface, normalize=True)  # вектор на отражающую площадку
+        xyz_magnet_line = matrix.get_xyz_coord(magnet_surface, normalize=True)  # вектор на отражающую площадку
         # -xyz потому что берем угол от нормали к НЗ
         # cos_alpha_matrix - это также матрица необходимая для расчета tau - как накрест лежащие
-        cos_alpha_matrix = np.einsum('ijl,ijl->ij', surface.array_normal, -xyz_magnet_line)
+        cos_alpha_matrix = np.einsum('ijl,ijl->ij', magnet_surface.array_normal, -xyz_magnet_line)
         # cos_alpha_1_matrix = np.einsum('ijl,ijl->ij', -surface.array_normal, xyz_magnet_line)
         new_cos_psi_range *= cos_alpha_matrix
 
         # УЧЕСТЬ tau в отражаемой точке!!!!!!!!!
         # tau_scatter_matrix = np.ones_like(cos_alpha_matrix)
-        tau_scatter_matrix = shadows.get_tau_for_scatter_with_cos(surface.theta_range, surface.surf_R_e,
+        tau_scatter_matrix = shadows.get_tau_for_scatter_with_cos(magnet_surface.theta_range, magnet_surface.surf_R_e,
                                                                   curr_configuration.M_accretion_rate, a_portion,
                                                                   cos_alpha_matrix)
 
-        L[i] = integralsService.calc_scatter_L(surface, curr_configuration.top_column.L_x, new_cos_psi_range,
+        L[i] = integralsService.calc_scatter_L(magnet_surface, curr_configuration.top_column.L_x, new_cos_psi_range,
                                                tau_scatter_matrix)
 
         # save
