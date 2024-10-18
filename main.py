2cb9c7b35949a8ec9d79cfecbd6234bf3e9cfa8c
Pochapil <bagrat37@mail.ru>
2024-10-18 22:15:45 +0300

    сохранение 'save_values.txt'

--- main.py
+++ main.py
@@ -5,11 +5,13 @@ import multiprocessing as mp
 
 import accretingNS
 import config
+import newService
 from geometry import matrix
 import pathService
 import shadows
 import integralsService
 import plot_package.plot_scripts
+import save
 
 
 def calc_shadows_and_tau(curr_configuration, surface, obs_matrix, mask_flag=False):
@@ -70,6 +72,14 @@ def calc_shadows_and_tau(curr_configuration, surface, obs_matrix, mask_flag=Fals
     return new_cos_psi_range
 
 
+def calc_number_pow(num):
+    pow = 0
+    while num > 10:
+        num = num / 10
+        pow += 1
+    return num, pow
+
+
 if __name__ == '__main__':
     mu = 0.1e31
     beta_mu = 40
@@ -83,7 +93,34 @@ if __name__ == '__main__':
 
     cur_dir_saved = pathService.PathSaver(mu, theta_obs, beta_mu, mc2, a_portion, phi_0)
     cur_path = cur_dir_saved.get_path()
-    # print(cur_path)
+    print(cur_path)
+    save.create_file_path(cur_path)
+
+    file_name = 'surfaces_T_eff.txt'
+    save.save_arr_as_txt(curr_configuration.top_column.T_eff, cur_path, file_name)
+
+    file_name = "save_phi_range.txt"
+    save.save_arr_as_txt(curr_configuration.top_column.outer_surface.phi_range, cur_path, file_name)
+
+    file_name = "save_theta_range.txt"
+    save.save_arr_as_txt(curr_configuration.top_column.outer_surface.theta_range, cur_path, file_name)
+
+    file_name = 'save_values.txt'
+    with open(cur_path / file_name, 'w') as f:
+        f.write(f'R_e = {curr_configuration.top_column.R_e / config.R_ns:.6f}\n')
+        f.write(f'ksi_shock = {curr_configuration.top_column.ksi_shock:.6f}\n')
+        f.write(f'beta = {curr_configuration.top_column.beta:.6f}\n')
+
+        number, power = calc_number_pow(curr_configuration.top_column.L_x)
+        f.write(f'total L_x = {number:.6f} * 10**{power}\n')
+
+        L_calc = integralsService.calculate_total_luminosity(curr_configuration.top_column.inner_surface,
+                                                             curr_configuration.top_column.T_eff)
+
+        f.write(f'difference L_x / L_calc - 1 : {(curr_configuration.top_column.L_x / L_calc - 1) * 100:.6f} %\n')
+
+        number, power = calc_number_pow(L_calc / 4)
+        f.write(f'calculated total L_x of single surface = {number:.6f} * 10**{power}\n')
 
     theta_obs_rad = np.deg2rad(theta_obs)
     beta_mu_rad = np.deg2rad(beta_mu)
@@ -102,18 +139,18 @@ if __name__ == '__main__':
 
         obs_matrix[phase_index] = e_obs_mu
 
+    T_eff = curr_configuration.top_column.T_eff
+    # print(integralsService.calculate_total_luminosity(curr_configuration.top_column.inner_surface, T_eff))
+    # print(curr_configuration.top_column.L_x)
+
     # чтобы соответствовать порядку в старом
     # surfaces = {0: top_column.outer_surface, 1: top_column.inner_surface,
     #             2: bot_column.outer_surface, 3: bot_column.inner_surface}
     accr_col_surfs = [curr_configuration.top_column.outer_surface, curr_configuration.top_column.inner_surface,
                       curr_configuration.bot_column.outer_surface, curr_configuration.bot_column.inner_surface]
 
-    T_eff = curr_configuration.top_column.T_eff
-    print(integralsService.calculate_total_luminosity(curr_configuration.top_column.inner_surface, T_eff))
-    print(curr_configuration.top_column.L_x)
-
-    L = np.empty((4, config.N_phase))
-    L_nu = np.empty((4, config.N_energy, config.N_phase))
+    L_surfs = np.empty((4, config.N_phase))
+    L_nu_surfs = np.empty((4, config.N_energy, config.N_phase))
 
     # PF_L = np.empty(4)
     # PF_L_nu = np.empty((4, config.N_energy))
@@ -129,31 +166,47 @@ if __name__ == '__main__':
         # new_cos_psi_range = calc_shadows_and_tau(curr_configuration, surface, obs_matrix)
         # print(new_cos_psi_range_async[i][np.abs(new_cos_psi_range_async[i] - new_cos_psi_range) > 1e-3].shape)
         new_cos_psi_range = new_cos_psi_range_async[i]
-        L[i] = integralsService.calc_L(surface, T_eff, new_cos_psi_range)
-        # save
-        L_nu[i] = integralsService.calc_L_nu(surface, T_eff, new_cos_psi_range)
-        # save
-        # calc PF
+        L_surfs[i] = integralsService.calc_L(surface, T_eff, new_cos_psi_range)
+        L_nu_surfs[i] = integralsService.calc_L_nu(surface, T_eff, new_cos_psi_range)
 
-        # PF_L[i] = integralsService.get_PF(L[i])
-        # PF_L_nu[i] = integralsService.get_PF(L_nu[i])
+    PF_L_surf = integralsService.get_PF(np.sum(L_surfs, axis=0))
+    PF_L_nu_surf = integralsService.get_PF(np.sum(L_nu_surfs, axis=0))
 
-    # print(L)
-    # print(L_nu)
+    plot_package.plot_scripts.plot_L(L_surfs)
 
-    PF_L = integralsService.get_PF(np.sum(L, axis=0))
-    PF_L_nu = integralsService.get_PF(np.sum(L_nu, axis=0))
+    file_name = 'save_values.txt'
+    with open(cur_path / file_name, 'a') as f:
+        avg_L_on_phase = np.mean(np.sum(L_surfs, axis=0))
+        number, power = calc_number_pow(avg_L_on_phase)
+        f.write(f'avg_L_on_phase = {number:.6f} * 10**{power}\n')
+        f.write(f'avg_L_on_phase / L_x = {avg_L_on_phase / curr_configuration.top_column.L_x:.3}\n')
 
-    plot_package.plot_scripts.plot_L(L)
+        d0 = newService.get_delta_distance(curr_configuration.top_column.inner_surface.theta_range[0],
+                                           curr_configuration.top_column.inner_surface.surf_R_e)
+        l0 = newService.get_A_normal(curr_configuration.top_column.inner_surface.theta_range[0],
+                                     curr_configuration.top_column.inner_surface.surf_R_e, a_portion) / d0
+
+        f.write(f'width / length = {(d0 / l0):.6}\n')
+        f.write(f'A_perp / 2 d0**2 = {(l0 / (2 * d0)):.6}\n')
+        f.write('L_data = dummy\n')
+        # f.write(f'L_data = {number:.6f} * 10**{power}\n')
+    # save
 
     magnet_line_surfs = [curr_configuration.top_magnet_lines, curr_configuration.bot_magnet_lines]
+    L_scatter = np.empty((2, config.N_phase))
+    L_nu_scatter = np.empty((2, config.N_energy, config.N_phase))
 
-    L = np.empty((2, config.N_phase))
-    L_nu = np.empty((2, config.N_energy, config.N_phase))
+    t1 = time.perf_counter()
+    with mp.Pool(processes=2) as pool:
+        new_cos_psi_range_async = pool.starmap(calc_shadows_and_tau,
+                                               zip(repeat(curr_configuration), magnet_line_surfs, repeat(obs_matrix),
+                                                   repeat(True)))
+    t2 = time.perf_counter()
+    print(f'{t2 - t1} seconds')
     # ------------------------------------------------ L_scatter ----------------------------------------------------
     for i, magnet_surface in enumerate(magnet_line_surfs):
-        new_cos_psi_range = calc_shadows_and_tau(curr_configuration, magnet_surface, obs_matrix, True)
-        # print(new_cos_psi_range)
+        # new_cos_psi_range = calc_shadows_and_tau(curr_configuration, magnet_surface, obs_matrix, True)
+        new_cos_psi_range = new_cos_psi_range_async[i]
 
         # УЧЕСТЬ угол падения от центра в отражаемой точке!!!!!!!!!
         xyz_magnet_line = matrix.get_xyz_coord(magnet_surface, normalize=True)  # вектор на отражающую площадку
@@ -171,10 +224,20 @@ if __name__ == '__main__':
 
         L_x = integralsService.calculate_total_luminosity(curr_configuration.top_column.inner_surface, T_eff)
         # надо брать не curr_configuration.top_column.L_x а посчитанный через интеграл! хоть они и отличаются на сильно
-        L[i] = integralsService.calc_scatter_L(magnet_surface, L_x, new_cos_psi_range, tau_scatter_matrix)
+        L_scatter[i] = integralsService.calc_scatter_L(magnet_surface, L_x, new_cos_psi_range, tau_scatter_matrix)
 
-        L_nu[i] = integralsService.calc_scatter_L_nu(magnet_surface, curr_configuration.top_column.inner_surface, T_eff,
-                                                     new_cos_psi_range, tau_scatter_matrix)
+        L_nu_scatter[i] = integralsService.calc_scatter_L_nu(magnet_surface,
+                                                             curr_configuration.top_column.inner_surface, T_eff,
+                                                             new_cos_psi_range, tau_scatter_matrix)
 
-    plot_package.plot_scripts.plot_L(L)
-    # print(L_nu)
+    plot_package.plot_scripts.plot_L(L_scatter)
+
+    PF_L_surf = integralsService.get_PF(np.sum(L_scatter, axis=0))
+    PF_L_nu_surf = integralsService.get_PF(np.sum(L_nu_scatter, axis=0))
+
+    file_name = 'save_values.txt'
+    with open(cur_path / file_name, 'a') as f:
+        number, power = calc_number_pow(avg_L_on_phase + np.mean(np.sum(L_scatter, axis=0)))
+        f.write(f'avg L with scatter = {number:.6f} * 10**{power}\n')
+        number = (avg_L_on_phase + np.mean(np.sum(L_scatter, axis=0))) / curr_configuration.top_column.L_x
+        f.write(f'avg L with scatter / L_x = {number:.6f}\n')
