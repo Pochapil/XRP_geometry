c20aa76d9fc6ce9405daf548f5c7c3202a2f3de9
Pochapil <bagrat37@mail.ru>
2025-01-31 15:18:24 +0300

    заготовленные массивы

--- main_loop.py
+++ main_loop.py
@@ -35,13 +35,13 @@ if __name__ == '__main__':
 
     # --------sky_map-------------
     theta_obs_arr = [10 * i for i in range(0, 10)]
-    beta_mu_arr = [10 * i for i in range(0, 10)]
+    beta_mu_arr = [10 * i for i in range(0, 9)]
     mc2_arr = [30, 60, 100]
     a_portion_arr = [0.22, 0.44, 0.66, 1]
     phi_0_arr = [0]
 
     # -----------L_to_m-----
-    theta_obs_arr = [20]
+    theta_obs_arr = [20, 40]
     beta_mu_arr = [60]
     # [10, 20, 30, 40, 50, 60, 70, 80, 90, 100, 110, 120, 130]
     mc2_arr = [10, 20, 40, 50, 70, 80, 90, 110, 120, 130]
@@ -50,8 +50,8 @@ if __name__ == '__main__':
 
     # ------L_to_a------
     theta_obs_arr = [20]
-    beta_mu_arr = [40]
-    mc2_arr = [100]
+    beta_mu_arr = [40]  # 40 60
+    mc2_arr = [100]  # 30 100
     a_portion_arr = [0.165, 0.22, 0.275, 0.33, 0.385, 0.44, 0.5, 0.55, 0.605, 0.66, 0.715, 0.77, 0.825]
     a_portion_arr = [0.165, 0.275, 0.33, 0.385, 0.5, 0.55, 0.605, 0.715, 0.77, 0.825]
     phi_0_arr = [0]
@@ -65,10 +65,46 @@ if __name__ == '__main__':
     phi_0_arr = [20, 40, 60, 80, 100, 120, 140, 160, 180]
 
     # -------cur-------
-    theta_obs_arr = [10 * i for i in range(0, 10)]
-    beta_mu_arr = [10 * i for i in range(0, 9)]
-    mc2_arr = [30, 60, 100]
-    a_portion_arr = [0.22, 0.44, 0.66, 1]
+    # theta_obs_arr = [20, 40, 60]
+    # beta_mu_arr = [20]
+    # mc2_arr = [30, 100]
+    # a_portion_arr = [0.22, 0.66]
+    # phi_0_arr = [0, 20, 40, 60, 80, 100, 120, 140, 160, 180]
+    # phi_0_arr = [20, 40, 60, 80, 100, 120, 140, 160, 180]
+
+    # theta_obs_arr = [20, 40, 60]
+    # beta_mu_arr = [20]
+    # # [10, 20, 30, 40, 50, 60, 70, 80, 90, 100, 110, 120, 130]
+    # mc2_arr = [10, 20, 40, 50, 70, 80, 90, 110, 120, 130]
+    # a_portion_arr = [0.22, 0.44, 0.66]
+    # phi_0_arr = [0, 20]
+
+    # theta_obs_arr = [20, 40, 60]
+    # beta_mu_arr = [20]  # 40 60
+    # mc2_arr = [30, 60, 100]  # 30 100
+    # a_portion_arr = [0.165, 0.22, 0.275, 0.33, 0.385, 0.44, 0.5, 0.55, 0.605, 0.66, 0.715, 0.77, 0.825]
+    # a_portion_arr = [0.165, 0.275, 0.33, 0.385, 0.5, 0.55, 0.605, 0.715, 0.77, 0.825]
+    # phi_0_arr = [20]
+
+    # theta_obs_arr = [20, 40]
+    # beta_mu_arr = [60]
+    # # [10, 20, 30, 40, 50, 60, 70, 80, 90, 100, 110, 120, 130]
+    # mc2_arr = [10, 20, 40, 50, 70, 80, 90, 110, 120, 130]
+    # a_portion_arr = [0.44, 0.66]
+    # phi_0_arr = [0]
+
+    # theta_obs_arr = [20]
+    # beta_mu_arr = [60]  # 40 60
+    # mc2_arr = [30]  # 30 100
+    # a_portion_arr = [0.165, 0.22, 0.275, 0.33, 0.385, 0.44, 0.5, 0.55, 0.605, 0.66, 0.715, 0.77, 0.825]
+    # a_portion_arr = [0.165, 0.275, 0.33, 0.385, 0.5, 0.55, 0.605, 0.715, 0.77, 0.825]
+    # phi_0_arr = [0]
+
+    theta_obs_arr = [60]
+    beta_mu_arr = [70]
+    # [10, 20, 30, 40, 50, 60, 70, 80, 90, 100, 110, 120, 130]
+    mc2_arr = [100]
+    a_portion_arr = [0.66]
     phi_0_arr = [0]
 
     plot_flag = True
@@ -76,7 +112,7 @@ if __name__ == '__main__':
     N_big = len(theta_obs_arr) * len(beta_mu_arr) * len(mc2_arr) * len(a_portion_arr) * len(phi_0_arr)
     if config.flag_calc_clever:
 
-        sec_one_loop = 80
+        sec_one_loop = 90
         print(
             f'{N_big} loops about {sec_one_loop * N_big / config.N_cpus:.2f} seconds or '
             + f'{sec_one_loop * N_big / 60 / config.N_cpus:.2f} mins or '
