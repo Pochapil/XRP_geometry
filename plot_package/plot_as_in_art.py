979ca14a71d866211cfd456064014673fb6fbb1a
Pochapil <bagrat37@mail.ru>
2025-04-18 14:06:29 +0300

    цикл + новые

--- plot_package/plot_as_in_art.py
+++ plot_package/plot_as_in_art.py
@@ -4,77 +4,20 @@ mu = 0.1e31
 
 
 def plot_sky_map_as_in_art():
-    beta_mu = 20
-    mc2 = 60
-    a_portion = 0.22
-    phi_0 = 0
-    plot_many_characteristics.plot_sky_map(mu, beta_mu, mc2, a_portion, phi_0)
 
     beta_mu = 20
     mc2 = 60
-    a_portion = 0.44
-    phi_0 = 0
-    plot_many_characteristics.plot_sky_map(mu, beta_mu, mc2, a_portion, phi_0)
+    a_portion_arr = [0.22, 0.66]
+    phi_0_arr = [0, 20, 40, 60]
+    for a_portion in a_portion_arr:
+        for phi_0 in phi_0_arr:
+            plot_many_characteristics.plot_sky_map(mu, beta_mu, mc2, a_portion, phi_0)
 
-    beta_mu = 20
-    mc2 = 60
-    a_portion = 0.66
-    phi_0 = 0
-    plot_many_characteristics.plot_sky_map(mu, beta_mu, mc2, a_portion, phi_0)
-
-    beta_mu = 20
-    mc2 = 60
-    a_portion = 0.22
-    phi_0 = 20
-    plot_many_characteristics.plot_sky_map(mu, beta_mu, mc2, a_portion, phi_0)
-
-    beta_mu = 20
-    mc2 = 60
-    a_portion = 0.44
-    phi_0 = 20
-    plot_many_characteristics.plot_sky_map(mu, beta_mu, mc2, a_portion, phi_0)
-
-    beta_mu = 20
-    mc2 = 60
-    a_portion = 0.66
-    phi_0 = 20
-    plot_many_characteristics.plot_sky_map(mu, beta_mu, mc2, a_portion, phi_0)
-
-    beta_mu = 20
-    mc2 = 60
-    a_portion = 0.22
-    phi_0 = 40
-    plot_many_characteristics.plot_sky_map(mu, beta_mu, mc2, a_portion, phi_0)
-
-    beta_mu = 20
-    mc2 = 60
-    a_portion = 0.44
-    phi_0 = 40
-    plot_many_characteristics.plot_sky_map(mu, beta_mu, mc2, a_portion, phi_0)
-
-    beta_mu = 20
-    mc2 = 60
-    a_portion = 0.66
-    phi_0 = 40
-    plot_many_characteristics.plot_sky_map(mu, beta_mu, mc2, a_portion, phi_0)
-
-    beta_mu = 20
-    mc2 = 60
-    a_portion = 0.22
-    phi_0 = 60
-    plot_many_characteristics.plot_sky_map(mu, beta_mu, mc2, a_portion, phi_0)
-
-    beta_mu = 20
-    mc2 = 60
-    a_portion = 0.44
-    phi_0 = 60
-    plot_many_characteristics.plot_sky_map(mu, beta_mu, mc2, a_portion, phi_0)
-
-    beta_mu = 20
-    mc2 = 60
-    a_portion = 0.66
-    phi_0 = 60
-    plot_many_characteristics.plot_sky_map(mu, beta_mu, mc2, a_portion, phi_0)
+    # beta_mu = 20
+    # mc2 = 60
+    # a_portion = 0.22
+    # phi_0 = 0
+    # plot_many_characteristics.plot_sky_map(mu, beta_mu, mc2, a_portion, phi_0)
 
 
 def plot_L_to_mc2_as_in_art():
@@ -153,8 +96,8 @@ def plot_L_to_a_portion_as_in_art():
 
 def plot_L_to_phi_0_as_in_art():
     theta_obs = 40
-    beta_mu = 60
-    mc2 = 30
+    beta_mu = 20
+    mc2 = 60
     a_portion = 0.22
     phi_0_arr = [0, 20, 40, 60, 80, 100, 120, 140, 160, 180]
     plot_many_characteristics.plot_L_to_phi_0(mu, theta_obs, beta_mu, mc2, a_portion, phi_0_arr, True)
@@ -164,11 +107,27 @@ def plot_L_to_phi_0_as_in_art():
     # plt.close()
 
     theta_obs = 40
-    beta_mu = 60
+    beta_mu = 20
+    mc2 = 60
+    a_portion = 0.66
+    phi_0_arr = [0, 20, 40, 60, 80, 100, 120, 140, 160, 180]
+    plot_many_characteristics.plot_L_to_phi_0(mu, theta_obs, beta_mu, mc2, a_portion, phi_0_arr, True)
+
+
+    theta_obs = 20
+    beta_mu = 20
+    mc2 = 30
+    a_portion = 0.22
+    phi_0_arr = [0, 20, 40, 60, 80, 100, 120, 140, 160, 180]
+    plot_many_characteristics.plot_L_to_phi_0(mu, theta_obs, beta_mu, mc2, a_portion, phi_0_arr, True)
+
+    theta_obs = 20
+    beta_mu = 20
     mc2 = 30
     a_portion = 0.66
     phi_0_arr = [0, 20, 40, 60, 80, 100, 120, 140, 160, 180]
     plot_many_characteristics.plot_L_to_phi_0(mu, theta_obs, beta_mu, mc2, a_portion, phi_0_arr, True)
+
 
 
 def plot_masses_PF_L_nu_as_in_art():
@@ -242,19 +201,9 @@ def plot_table_as_in_art():
     plot_many_characteristics.plot_table_together(mu, theta_obs, beta_mu, mc2_arr, a_portion_arr, phi_0)
 
 if __name__ == "__main__":
-    beta_mu = 20
-    mc2 = 30
-    a_portion_arr = [0.22, 0.66]
-    phi_0_arr = [20, 40, 60]
-    for a_portion in a_portion_arr:
-        for phi_0 in phi_0_arr:
-            pass
-            # plot_many_characteristics.plot_sky_map(mu, beta_mu, mc2, a_portion, phi_0)
-
-
+    ...
     # plot_sky_map_as_in_art()
-    # plot_sky_map_as_in_art()
-    # plot_L_to_phi_0_as_in_art()
+    plot_L_to_phi_0_as_in_art()
     # plot_L_to_mc2_as_in_art()
     # plot_L_to_a_portion_as_in_art()
     # plot_masses_PF_L_nu_as_in_art()
