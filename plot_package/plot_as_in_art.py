ffad4d19d87de4237f5f32b5bf6b56ba65886a1d
Pochapil <bagrat37@mail.ru>
2025-05-15 12:47:23 +0300

    изменил домен? рассматриваемые значения

--- plot_package/plot_as_in_art.py
+++ plot_package/plot_as_in_art.py
@@ -1,13 +1,13 @@
 import plot_many_characteristics
+import numpy as np
 
 mu = 0.1e31
 
 
 def plot_sky_map_as_in_art():
-
     beta_mu = 20
     mc2 = 60
-    a_portion_arr = [0.22, 0.44, 0.66]
+    a_portion_arr = [0.2, 0.4, 0.6, 0.25, 0.5, 0.75]
     phi_0_arr = [0, 20, 40, 60]
     for a_portion in a_portion_arr:
         for phi_0 in phi_0_arr:
@@ -51,47 +51,22 @@ def plot_L_to_mc2_as_in_art():
 
 
 def plot_L_to_a_portion_as_in_art():
-    theta_obs = 20
-    beta_mu = 40
-    mc2 = 100
-    a_portion_arr = [0.165, 0.22, 0.275, 0.33, 0.385, 0.44, 0.5, 0.55, 0.605, 0.66, 0.715, 0.77, 0.825, 1]
-    phi_0 = 0
-    plot_many_characteristics.plot_L_to_a_portion(mu, theta_obs, beta_mu, mc2, a_portion_arr, phi_0)
+    # theta_obs = 20
+    # beta_mu = 60
+    # mc2 = 30
+    # a_portion_arr = [0.165, 0.22, 0.275, 0.33, 0.385, 0.44, 0.5, 0.55, 0.605, 0.66, 0.715, 0.77, 0.825, 1]
+    # phi_0 = 0
+    # plot_many_characteristics.plot_L_to_a_portion(mu, theta_obs, beta_mu, mc2, a_portion_arr, phi_0)
 
-    theta_obs = 20
-    beta_mu = 60
-    mc2 = 30
-    a_portion_arr = [0.165, 0.22, 0.275, 0.33, 0.385, 0.44, 0.5, 0.55, 0.605, 0.66, 0.715, 0.77, 0.825, 1]
-    phi_0 = 0
-    plot_many_characteristics.plot_L_to_a_portion(mu, theta_obs, beta_mu, mc2, a_portion_arr, phi_0)
-
-    theta_obs = 40
-    beta_mu = 20
-    mc2 = 30
-    a_portion_arr = [0.165, 0.22, 0.275, 0.33, 0.385, 0.44, 0.5, 0.55, 0.605, 0.66, 0.715, 0.77, 0.825, 1]
-    phi_0 = 0
-    plot_many_characteristics.plot_L_to_a_portion(mu, theta_obs, beta_mu, mc2, a_portion_arr, phi_0)
-
-    theta_obs = 40
-    beta_mu = 20
-    mc2 = 100
-    a_portion_arr = [0.165, 0.22, 0.275, 0.33, 0.385, 0.44, 0.5, 0.55, 0.605, 0.66, 0.715, 0.77, 0.825, 1]
-    phi_0 = 0
-    plot_many_characteristics.plot_L_to_a_portion(mu, theta_obs, beta_mu, mc2, a_portion_arr, phi_0)
-
-    theta_obs = 60
-    beta_mu = 20
-    mc2 = 30
-    a_portion_arr = [0.165, 0.22, 0.275, 0.33, 0.385, 0.44, 0.5, 0.55, 0.605, 0.66, 0.715, 0.77, 0.825, 1]
-    phi_0 = 0
-    plot_many_characteristics.plot_L_to_a_portion(mu, theta_obs, beta_mu, mc2, a_portion_arr, phi_0)
-
-    theta_obs = 60
-    beta_mu = 20
-    mc2 = 100
-    a_portion_arr = [0.165, 0.22, 0.275, 0.33, 0.385, 0.44, 0.5, 0.55, 0.605, 0.66, 0.715, 0.77, 0.825, 1]
-    phi_0 = 0
-    plot_many_characteristics.plot_L_to_a_portion(mu, theta_obs, beta_mu, mc2, a_portion_arr, phi_0)
+    theta_obs_arr = [20, 40, 60]
+    beta_mu_arr = [20, 60]
+    mc2 = 60
+    phi_0_arr = [0, 40]
+    a_portion_arr = np.linspace(0.1, 1, 19)
+    for theta_obs in theta_obs_arr:
+        for beta_mu in beta_mu_arr:
+            for phi_0 in phi_0_arr:
+                plot_many_characteristics.plot_L_to_a_portion(mu, theta_obs, beta_mu, mc2, a_portion_arr, phi_0)
 
 
 def plot_L_to_phi_0_as_in_art():
@@ -113,7 +88,6 @@ def plot_L_to_phi_0_as_in_art():
     phi_0_arr = [0, 20, 40, 60, 80, 100, 120, 140, 160, 180]
     plot_many_characteristics.plot_L_to_phi_0(mu, theta_obs, beta_mu, mc2, a_portion, phi_0_arr, True)
 
-
     theta_obs = 20
     beta_mu = 20
     mc2 = 30
@@ -127,7 +101,6 @@ def plot_L_to_phi_0_as_in_art():
     a_portion = 0.66
     phi_0_arr = [0, 20, 40, 60, 80, 100, 120, 140, 160, 180]
     plot_many_characteristics.plot_L_to_phi_0(mu, theta_obs, beta_mu, mc2, a_portion, phi_0_arr, True)
-
 
 
 def plot_masses_PF_L_nu_as_in_art():
@@ -200,12 +173,13 @@ def plot_table_as_in_art():
     a_portion_arr = [0.22, 0.44, 0.66, 1]
     plot_many_characteristics.plot_table_together(mu, theta_obs, beta_mu, mc2_arr, a_portion_arr, phi_0)
 
+
 if __name__ == "__main__":
     ...
-    plot_sky_map_as_in_art()
+    # plot_sky_map_as_in_art()
     # plot_L_to_phi_0_as_in_art()
     # plot_L_to_mc2_as_in_art()
-    # plot_L_to_a_portion_as_in_art()
+    plot_L_to_a_portion_as_in_art()
     # plot_masses_PF_L_nu_as_in_art()
     # plot_PF_contour_as_in_art()
     # plot_table_as_in_art()
